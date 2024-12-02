028b15ae2664dc85fd91c1793dfb63f93368bc58
kiiramat <kiiramati@gmail.com>
2024-12-02 17:45:33 +0000

    Add images and sort them

---
+++ js/app.js
@@ -0,0 +1,52 @@
+const cardArray = [
+    {
+        name: "cheeseburger",
+        img: "images/cheeseburger.png"
+    },
+    {
+        name: "fries",
+        img: "images/fries.png"
+    },
+    {
+        name: "hotdog",
+        img: "images/hotdog.png"
+    },
+    {
+        name: "ice-cream",
+        img: "images/ice-cream.png"
+    },
+    {
+        name: "milshake",
+        img: "images/milshake.png"
+    },
+    {
+        name: "pizza",
+        img: "images/pizza.png"
+    },
+    {
+        name: "cheeseburger",
+        img: "images/cheeseburger.png"
+    },
+    {
+        name: "fries",
+        img: "images/fries.png"
+    },
+    {
+        name: "hotdog",
+        img: "images/hotdog.png"
+    },
+    {
+        name: "ice-cream",
+        img: "images/ice-cream.png"
+    },
+    {
+        name: "milshake",
+        img: "images/milshake.png"
+    },
+    {
+        name: "pizza",
+        img: "images/pizza.png"
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random());
